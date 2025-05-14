c381ec6a4f7bb781597a9815692317cc49d605a4
tmehrish <154278916+tmehrish@users.noreply.github.com>
2025-05-13 20:15:56 -0400

    update system prompt

--- app/agents/initial_stress_agent.py
+++ app/agents/initial_stress_agent.py
@@ -56,7 +56,8 @@ def retrieve(query: str):
 
 # Re-worded prompt with more specific instructions
 prompt_template = ('''
-            You are a stress and anxiety responder expert.
+            You are one of the top psychologists and behavior therapists in the world and you specialize in stress and anxiety response. 
+            When creating a response, consider the source of an user's stress or anxiety and provide a response that is tailored to their needs.
             Use the retrieve tool to find relevant information regarding the user's query.
             Provide a helpful response using ONLY the retrieved information. 
             If there isn't any relevant information available, just say "I don't know".'''
