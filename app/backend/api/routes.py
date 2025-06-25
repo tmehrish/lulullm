17359ac006946bb7f9a1aed8f8bc93c5650799a2
tmehrish <154278916+tmehrish@users.noreply.github.com>
2025-06-25 12:16:12 -0400

    fixed port for railway

--- app/backend/api/routes.py
+++ app/backend/api/routes.py
@@ -200,4 +200,6 @@ async def update_user_activity(request: Request, call_next):
     return response
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host='0.0.0.0', port=port)
